d42952ad6f5637104c3370b90c6cc76bf8c6f7a9
eekokok <yeall0324@gmail.com>
2026-05-12 21:10:44 +0800

    20260512

--- WinFormsApp2/WinFormsApp2/Form2.cs
+++ WinFormsApp2/WinFormsApp2/Form2.cs
@@ -24,9 +24,29 @@ namespace WinFormsApp2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int c = int.Parse(textBox1.Text);
-            double f = c*(9/5.0)+32;
-            label3.Text = f.ToString()+"度F";
+            try
+            {
+                int[] numbers = new int[10];
+                numbers[0]=1;
+                numbers[1]=2;
+                MessageBox.Show(numbers[10].ToString())
+;                int c = int.Parse(textBox1.Text);
+                double f = c*(9/5.0)+32;
+                label3.Text = f.ToString()+"度F";
+            }
+            catch(FormatException ex)
+            {
+                MessageBox.Show(ex.Message+"輸入的不可以是標點符號，請輸入整數");
+            }catch(IndexOutOfRangeException ex)
+            {
+                MessageBox.Show(ex.Message+"你宣告的陣列爆掉了拉!");
+            } catch (System.Exception ex) {
+           
+                MessageBox.Show(ex.Message+"你輸入的有問題，但我不知道哪裡錯");
+            }
+
         }
+            
+        
     }
 }
