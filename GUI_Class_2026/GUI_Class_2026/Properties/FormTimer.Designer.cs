3262cbb4d762cf4c27507ea68c6e2febd5368085
eekokok <yeall0324@gmail.com>
2026-05-29 13:03:28 +0800

    20260529

--- GUI_Class_2026/GUI_Class_2026/Properties/FormTimer.Designer.cs
+++ GUI_Class_2026/GUI_Class_2026/Properties/FormTimer.Designer.cs
@@ -31,6 +31,11 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.labcounter = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
@@ -42,21 +47,73 @@
             // labcounter
             // 
             this.labcounter.AutoSize = true;
-            this.labcounter.Location = new System.Drawing.Point(96, 56);
+            this.labcounter.Location = new System.Drawing.Point(128, 70);
+            this.labcounter.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labcounter.Name = "labcounter";
-            this.labcounter.Size = new System.Drawing.Size(11, 12);
+            this.labcounter.Size = new System.Drawing.Size(14, 15);
             this.labcounter.TabIndex = 0;
             this.labcounter.Text = "0";
             this.labcounter.Click += new System.EventHandler(this.labcounter_Click);
             // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(413, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(522, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "暫停";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(305, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "歸零";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("新細明體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(424, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0秒";
+            // 
             // FormTimer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.labcounter);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormTimer";
             this.Text = "FormTimer";
+            this.Load += new System.EventHandler(this.FormTimer_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,5 +123,10 @@
 
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label labcounter;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
--- GUI_Class_2026/GUI_Class_2026/Properties/FormTimer.cs
+++ GUI_Class_2026/GUI_Class_2026/Properties/FormTimer.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -28,6 +29,34 @@ namespace GUI_Class_2026.Properties
             int counter = Int32.Parse(labcounter.Text.Replace("秒", ""));
             counter+=1;
             labcounter.Text = counter.ToString()+"秒";
+        }
+
+        private void FormTimer_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           timer2.Enabled = false;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            int timer = Int32.Parse(label1.Text.Replace("秒", ""));
+            timer += 1;
+            label1.Text = timer.ToString() + "秒";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "0秒";
+            timer2.Enabled = false;
         }
     }
 }
